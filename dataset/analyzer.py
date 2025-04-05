dfeb17fb4a61ea370e0e9c823efd183ba7800a7c
N-Shimoda <naoki.shimoda@outlook.jp>
2025-04-05 06:57:49 +0000

    modified json structure

--- dataset/analyzer.py
+++ dataset/analyzer.py
@@ -1,26 +1,21 @@
 import json
-from collections import defaultdict
 
 
-def analyze_questions(filename: str):
-    # Load the JSON file
-    with open(filename, "r", encoding="utf-8") as f:
-        data = json.load(f)
+def analyze_questions(data: dict[str, dict]):
 
-    # Dictionary to count questions per category
-    category_counts = defaultdict(int)
-
-    # Count based on the prefix before the dash in the "id"
-    for item in data:
-        prefix = item["id"].split("-")[0]
-        category_counts[prefix] += 1
-
-    # Output results
-    print("Number of questions per category:")
-    for category, count in sorted(category_counts.items()):
-        print(f"{category}: {count}")
-    print("(Total: {})".format(len(data)))
+    category_counts = {cat: len(data[cat]["questions"]) for cat in data.keys()}
+    print(
+        "Number of MCQs in each category:\n\t{}\n\ttotal = {}".format(
+            category_counts, sum(category_counts.values())
+        )
+    )
 
 
 if __name__ == "__main__":
-    analyze_questions(filename="dataset/MCQs.json")
+
+    # Load the JSON file
+    filename = "dataset/MCQs.json"
+    with open(filename, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    analyze_questions(data)
