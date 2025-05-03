f98adae4bf2daad834fef3f8061c1e67c7bbe1ff
N-Shimoda <naoki.shimoda@outlook.jp>
2025-05-03 12:27:48 +0900

    plotted the distribution of word count

--- dataset/analyzer.py
+++ dataset/analyzer.py
@@ -1,27 +1,56 @@
 import json
-import sys
+import os
+
+import matplotlib.pyplot as plt
 
 
-def analyze_questions(data: dict[str, dict]):
+def plot_stats(stats: dict[str, dict]):
+    # define data
+    categories = list(stats["num_words"].keys())
+    word_counts = [stats["num_words"][cat] for cat in categories]
 
-    category_counts = {cat: len(data[cat]["questions"]) for cat in data.keys()}
-    print(
-        "Number of MCQs in each category:\n\t{}\n\ttotal = {}".format(
-            category_counts, sum(category_counts.values())
-        )
-    )
+    # boxplot
+    plt.figure(figsize=(10, 6))
+    plt.boxplot(word_counts, tick_labels=categories, patch_artist=True)
+
+    # graph settings
+    plt.title("Number of Words per Category")
+    plt.xlabel("Categories")
+    plt.ylabel("Word Count")
+    plt.grid(True, axis="y")
+
+    plt.tight_layout()
+
+    # save the plot
+    OUT_DIR = os.path.join(DS_DIR, "stats")
+    os.makedirs(OUT_DIR, exist_ok=True)
+    plt.savefig(f"{OUT_DIR}/{file.split('.')[0]}.svg", format="svg")
+
+
+def count_stats(filename: str) -> dict[str, dict[str, any]]:
+    with open(filename, "r") as f:
+        data: dict[str, dict] = json.load(f)
+
+    categories = data.keys()
+    stats = dict()
+
+    # word count
+    stats["num_words"] = {
+        cat: [len(entry["sentence"].split(" ")) for entry in data[cat]["questions"]] for cat in categories
+    }
+    return stats
 
 
 if __name__ == "__main__":
 
-    # Load the JSON file
-    try:
-        filename = sys.argv[1]
-    except IndexError:
-        print("Usage: python analyzer.py <filename>")
-        sys.exit(1)
+    DS_DIR = "dataset"
+    files = os.listdir(DS_DIR)
 
-    with open(filename, "r", encoding="utf-8") as f:
-        data = json.load(f)
+    for file in sorted(files):
+        if not file.endswith(".json"):
+            continue
+        stats = count_stats(os.path.join(DS_DIR, file))
+        print(f"{file.split('.')[0]}: {stats}")
 
-    analyze_questions(data)
+        # plot stats
+        plot_stats(stats)
