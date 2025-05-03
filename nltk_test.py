37c368744041f66b0bca7ba79a88cb8d01af5925
N-Shimoda <naoki.shimoda@outlook.jp>
2025-05-03 16:45:14 +0900

    removed unused codes

--- nltk_test.py
+++
@@ -1,9 +0,0 @@
-from nltk.tokenize import sent_tokenize
-
-text = (
-    "Hi, Mr. Owen. I'm Naoki Shimoda working at PKSHA Technology Inc. in Tokyo."
-    "This is the first sentence. Here's the second one! Is this the third? Yes, it is."
-)
-sentences = sent_tokenize(text)
-print(text)
-print(sentences)
