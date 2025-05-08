23101a5dbc386615714a0ed2ab2386c1b4fc1088
N-Shimoda <naoki.shimoda@outlook.jp>
2025-05-08 16:55:37 +0900

    modified script file for MCQA

---
+++ scripts/mcqa_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for MODEL in rebel unirel
+do
+    for ds in ./dataset/*.json
+    do
+        if [[ $(basename "$ds") != "dev.json" ]]; then
+            echo "Running MCQA on $ds (model: $MODEL)"
+            python mcqa.py $ds $MODEL
+            if [ $? -ne 0 ]; then
+                echo "Error: MCQA failed on $ds (model: $MODEL)" >&2
+            fi
+        fi
+    done
+done
