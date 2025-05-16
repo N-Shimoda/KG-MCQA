fcdc9b81fd8e0980609bf091c5224b91ab659b55
N-Shimoda <naoki.shimoda@outlook.jp>
2025-05-16 18:14:08 +0900

    updated get request with extracts of the article

--- wiki_test.py
+++ wiki_test.py
@@ -3,24 +3,35 @@ import json
 import requests
 
 
-def get_wikipedia_page_url(title: str, lang: str = "en") -> str:
+def get_wikipedia_page_summary(title: str, lang: str = "en") -> dict:
     url = f"https://{lang}.wikipedia.org/w/api.php"
-    params = {"action": "query", "format": "json", "titles": title, "prop": "info", "inprop": "url", "redirects": 1}
+    params = {
+        "action": "query",
+        "prop": "info|extracts",
+        "inprop": "url",
+        "exintro": 1,  # 冒頭部分のみ
+        "explaintext": 1,  # プレーンテキストで取得
+        "titles": title,
+        "redirects": 1,
+        "format": "json",
+    }
 
     response = requests.get(url, params=params)
     data = response.json()
 
     with open("response.json", "w") as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+        json.dump(data, f, indent=4)
 
     pages = data["query"]["pages"]
-    page = next(iter(pages.values()))
-
-    if "fullurl" in page:
-        return page["fullurl"]
-    else:
-        return f"記事『{title}』は見つかりませんでした。"
+    summaries = {}
+    for key, page in pages.items():
+        if int(key) > 0:
+            summaries[page["title"]] = page.get("extract", "")
+        else:
+            summaries[page["title"]] = None
+    return summaries
 
 
 # 例
-print(get_wikipedia_page_url("Kyoto University|Parasite|woodwind|Cubist Movement"))
+targets = ["Inception", "Kyoto University", "Parasite", "woodwind", "Cubist Movement", "harmony"]
+print(get_wikipedia_page_summary("|".join(targets)))
