7a015d6f29f9461e4e4a0584e6137c652a2d3255
N-Shimoda <naoki.shimoda@outlook.jp>
2025-05-25 21:31:27 +0900

    conducted MCQA with mREBEL_32

--- scripts/mcqa_all.sh
+++ scripts/mcqa_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-log_file="exp-mcqa/mcqa.log"
+log_file="log/mcqa.log"
 if [ -f "$log_file" ]; then
     rm "$log_file"
 fi
 
-api_log_file="exp-mcqa/wiki_api.log"
+api_log_file="log/wiki_api.log"
 if [ -f "$api_log_file" ]; then
     rm "$api_log_file"
 fi
